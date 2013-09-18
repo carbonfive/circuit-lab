19d920d31c43cbc36cf2ac662c1a2778555a180d
Ingar Shu <ingar@isnd.net>
2013-09-17 22:15:24 -0700

    - Draw dark LEDs in the simulator - Added some instructional text about the "API"

--- public/led_grid.js
+++ public/led_grid.js
@@ -1,10 +1,8 @@
 'use strict';
 
-function LEDGrid(canvas, rows, columns, grid) {
+function LEDGrid(canvas, panel) {
   this.ctx = canvas.getContext('2d');
-  this.rows = rows;
-  this.columns = columns;
-  this.grid = grid;
+  this.panel = panel;
 }
 
 LEDGrid.prototype.clear = function() {
@@ -13,7 +11,7 @@ LEDGrid.prototype.clear = function() {
 };
 
 LEDGrid.prototype.renderLEDs = function() {
-  this.ledSpacing = this.width / this.columns;
+  this.ledSpacing = this.width / this.panel.columns;
   this.ledRadius = this.ledSpacing * 0.1;
 
   this.ledGradient = this.ctx.createRadialGradient(0, 0, this.ledRadius * 0.25, 0, 0, this.ledRadius);
@@ -21,18 +19,21 @@ LEDGrid.prototype.renderLEDs = function() {
   this.ledGradient.addColorStop(1, '#00BFFF');
   this.ctx.fillStyle = this.ledGradient;
 
-  for (var row = 0; row < this.rows; row++) {
-    for (var col = 0; col < this.columns; col++) {
-      if (this.grid[row * this.columns + col])
-        this.illuminateLED(row, col);
+  for (var row = 0; row < this.panel.rows; row++) {
+    for (var col = 0; col < this.panel.columns; col++) {
+      if (this.panel.buf[row * this.panel.columns + col])
+        this.drawLED(row, col, this.ledGradient);
+      else
+        this.drawLED(row, col, '#444');
     }
   }
 };
 
-LEDGrid.prototype.illuminateLED = function(row, column) {
+LEDGrid.prototype.drawLED = function(row, column, fill) {
   var x = this.ledSpacing * column + this.ledSpacing * 0.5;
   var y = this.ledSpacing * row + this.ledSpacing * 0.5;
   this.ctx.save();
+    this.ctx.fillStyle = fill;
     this.ctx.translate(x, y);
     this.ctx.beginPath();
     this.ctx.arc(0, 0, this.ledRadius, 0, Math.PI * 2.0);
--- public/simulator.js
+++ public/simulator.js
@@ -2,69 +2,33 @@
 
 // LED at (x, y) is at grid[y * columns + x]
 // 0, 0 is upper left
-/*
-function test(state, rows, columns, grid) {
-  if (!state.initialized) {
-    state.x = 0;
-    state.y = 0;
-    state.dx = 1;
-    state.dy = 1;
-    state.initialized = true;
-  }
-
-  var now = parseInt(state.time / 100);
-
-// Update only every second
-  if (now == state.lastUpdate) {
-    return;
-  }
-
-// clear the grid
-  for (var i = 0; i < grid.length; i++) {
-    grid[i] = 0;
-  }
-
-  if ((state.x + state.dx >= columns) || (state.x + state.dx < 0)) {
-    state.dx = -state.dx;
-  }
-
-  if ((state.y + state.dy >= rows) || (state.y + state.dy < 0)) {
-    state.dy = -state.dy;
-  }
-
-  state.x += state.dx;
-  state.y += state.dy;
-  grid[state.y * columns + state.x] = 1;
-
-  state.lastUpdate = now;
-}
-
-var testFnStr = test.toString().replace(/function \w+\(.*?\) {/, '').replace(/}$/, '');
-*/
-
-
 
 $(function() {
-
   var editor = ace.edit('editor');
 
-  var gridRows = 16;
-  var gridCols = 32;
+  var ledPanel = {
+    setSize: function(rows, columns) {
+      this.rows = rows;
+      this.columns = columns;
+      this.buf = new Uint8Array(rows * columns)
+    }
+  };
+  ledPanel.setSize(16, 32);
+
   var userState = {};
-  var buf = new Uint8Array(gridRows * gridCols);
 
   var canvas = document.querySelector('#led-grid');
   var canvasWidth = canvas.clientWidth;
 
-  var grid = window.grid = new LEDGrid(canvas, gridRows, gridCols, buf);
+  var grid = new LEDGrid(canvas, ledPanel);
 
   function resizeCanvas(force) {
     if (force || (canvasWidth != canvas.clientWidth)) {
       console.log('Resizing canvas.');
       canvasWidth = canvas.clientWidth;
       canvas.width = canvasWidth;
-      canvas.height = canvasWidth / (gridCols / gridRows);
-      grid.render(gridRows, gridCols, buf);
+      canvas.height = canvasWidth / (ledPanel.columns / ledPanel.rows);
+      grid.render();
     }
   }
 
@@ -72,7 +36,7 @@ $(function() {
   resizeCanvas(true);
 
   function generateGenerator(fnStr) {
-    return new Function('state', 'rows', 'columns', 'grid', fnStr);
+    return new Function('state', 'time', 'rows', 'columns', 'grid', fnStr);
   }
   var generateFn = generateGenerator('');
 
@@ -85,12 +49,11 @@ $(function() {
     });
 
     // Grid dimensions form
-    $('#grid-columns').val(gridCols);
-    $('#grid-rows').val(gridRows);
+    $('#grid-columns').val(ledPanel.columns);
+    $('#grid-rows').val(ledPanel.rows);
     $('#grid-dimensions').on('submit', function(e) {
       e.preventDefault();
-      gridCols = parseInt($('#grid-columns').val());
-      gridRows = parseInt($('#grid-rows').val());
+      ledPanel.setSize(parseInt($('#grid-rows').val()), parseInt($('#grid-columns').val()));
       resizeCanvas(true);
     });
 
@@ -104,8 +67,7 @@ $(function() {
 
 
   function tick() {
-    userState.time = new Date().getTime();
-    generateFn(userState, gridRows, gridCols, buf);
+    generateFn(userState, new Date().getTime(), ledPanel.rows, ledPanel.columns, ledPanel.buf);
     grid.render();
     setTimeout(tick, 20);
   }
